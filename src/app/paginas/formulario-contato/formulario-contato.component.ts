161cf290a549d3597663149a82a48294b8304c47
Eriston Lima <eristonlima@gmail.com>
2026-08-05 20:49:12 -0300

    versao inicial

---
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Nay'),
+      telefone: new FormControl('99 99999-9999'),
+      email: new FormControl('nay@email.com'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('Olá, Mundo!!')
+    })
+  }
+
+  salvarContato() {
+    console.log(this.contatoForm.value)
+  }
+
+  cancelar() {
+    console.log('Submissão cancelada')
+  }
+}
